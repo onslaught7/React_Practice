ac3d109e30f68eaf13e7bacad7c7883b27678f96
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-21 16:48:01 +0530

    navigation bar, navbar react component with css

--- project_2/src/App.jsx
+++ project_2/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar';
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  return 
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
 }
 
 export default App
---
+++ project_2/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="nav-logo">
+        <img src="/images/pngegg.png" alt="contact page logo"/>
+        </div>
+
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ul>
+
+
+    </nav>
+  )
+}
+
+export default Navbar
