cf053a54a107b058f75f926d0115b1291111b60f
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-21 17:16:06 +0530

    added contact header .jsx and module.css

--- project_2/src/App.jsx
+++ project_2/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <Navbar/>
+      <contactHeader />
     </div>
   )
 }
---
+++ project_2/src/components/contactHeader/contactHeader.jsx
@@ -0,0 +1,11 @@
+import styles from "./contactHeader.module.css";
+
+const contactHeader = () => {
+    return (
+        <div className="container">
+
+        </div>
+    );
+};
+
+export default contactHeader;
