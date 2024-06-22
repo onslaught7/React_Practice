20c87182307b8ca79fb20219392ce62157ad8553
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-22 14:37:57 +0530

    Destructuring best practices

--- project_2/src/components/Button/Button.jsx
+++ project_2/src/components/Button/Button.jsx
@@ -1,7 +1,6 @@
 import styles from "./Button.module.css";
 
-const Button = (props) => {
-  const {isBtn1, icon, text} = props;
+const Button = ({isBtn1, icon, text}) => {
   return ( 
     <button className={isBtn1 ? styles.primary_btn_1 : styles.primary_btn_2}
     >
@@ -12,3 +11,18 @@ const Button = (props) => {
 };
 
 export default Button;
+
+// the above function is the same as the below
+// const Button = (props) => {
+//   const {isBtn1, icon, text} = props;
+  
+//   return ( 
+//     <button className={isBtn1 ? styles.primary_btn_1 : styles.primary_btn_2}
+//     >
+//        {icon}
+//        {text}
+//     </button>
+//   );
+// };
+
+// export default Button;
