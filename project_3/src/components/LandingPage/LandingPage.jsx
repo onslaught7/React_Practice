6b78b645dd0dd44e76c00b68c5f3ddb878537cd4
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-23 21:02:34 +0530

    centre align on page and button component left to complete landing page

--- project_3/src/components/LandingPage/LandingPage.jsx
+++ project_3/src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from './LandingPage.module.css'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className={`${styles.conainer_LandingPage} container`}>
+        <div className={styles.img_dice}>
+            <img src="/images/dices 1.png"/>
+        </div>
+
+        <div className={styles.text_dice}>
+            <p>DICE GAME</p>
+        </div>
+    </div>
   );
 };
 
--- project_3/src/components/PlayPage/PlayPage.jsx
+++ project_3/src/components/PlayPage/PlayPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PlayPage = () => {
   return (
-    <div>PlayPage</div>
+    <div></div>
   );
 };
 
