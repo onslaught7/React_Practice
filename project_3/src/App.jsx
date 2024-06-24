15dc2bcc49964b182a0ee0ab3e5e17f351d2bd07
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-24 22:32:04 +0530

    figured out how to add buttons through map function

--- project_3/src/App.jsx
+++ project_3/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <LandingPage />
+      {/* <LandingPage /> */}
       <PlayPage />
     </div>
   );
--- project_3/src/components/PlayPage/PlayPage.jsx
+++ project_3/src/components/PlayPage/PlayPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import styles from './PlayPage.module.css'
+import Button from '../Button/Button'
 
 const PlayPage = () => {
+  const btnNums = [1, 2, 3, 4, 5, 6];
+
   return (
-    <div></div>
+    <div className={`${styles.playpage_container} container`}>
+      <div className={styles.top}>
+        <div className={styles.score}>
+          <h1>0</h1>
+          <p>Total Score</p>
+        </div>
+
+        <div className={styles.num_select}>
+          {btnNums.map(number => (
+            <button key={number}>{number}</button>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.play_area}>
+
+      </div>
+    </div>
   );
 };
 
