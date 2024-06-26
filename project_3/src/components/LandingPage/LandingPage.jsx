af77785f334c17cc57459cbde7aceaf08571dc4c
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-26 18:57:27 +0530

    added animations as well to loading page

--- project_3/src/components/LandingPage/LandingPage.jsx
+++ project_3/src/components/LandingPage/LandingPage.jsx
@@ -3,11 +3,7 @@ import styles from './LandingPage.module.css'
 import Button from '../Button/Button'
 import { BsDice1 } from "react-icons/bs";
 
-const LandingPage = () => {
-  function handleClick() {
-    
-  }
-
+const LandingPage = ({ onPlayClick }) => {
   return (
     <div className={`${styles.conainer_LandingPage} container`}>
         <div className={styles.img_dice}>
@@ -17,7 +13,7 @@ const LandingPage = () => {
         <div className={styles.text_dice}>
             <p>DICE GAME</p>
             <Button
-              onClick={handleClick}
+              onClick={onPlayClick}
               icon={<BsDice1 fontSize="20px"/>}
               text="PLAY NOW"
             />
