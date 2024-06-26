49a43ffa55b6b6565908c32328447943fc6e4cb7
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-26 17:04:39 +0530

    front end design completed functionality to be added

--- project_3/src/components/PlayPage/PlayPage.jsx
+++ project_3/src/components/PlayPage/PlayPage.jsx
@@ -35,6 +35,14 @@ const PlayPage = () => {
                 text="Show Rules"
               />
             </div>
+
+            <div className={styles.rules}>
+              <h3>How to play dice game</h3>
+              <p>Select any number</p>
+              <p>Click on dice image</p>
+              <p>after click on  dice  if selected number is equal to dice number you will get same point as dice after click on  dice  if selected number is equal to dice number you will get same point as dice </p>
+              <p>if you get wrong guess then  2 point will be dedcuted</p>
+            </div>
       </div>
     </div>
   );
