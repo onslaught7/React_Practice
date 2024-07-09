85df8541a6fad6895649ccdf98eabc49c7e93b58
onslaught7 <krutibashmohapatra7@gmail.com>
2024-07-09 20:44:11 +0530

    linked the css and description

--- project_4/src/components/Description/Description.jsx
+++ project_4/src/components/Description/Description.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import styles from './Description.module.css';
 
 const Description = () => {
   return (
     <div>
       <Navbar />
+
+      <div className={styles.descPage}>
+        
+      </div>
     </div>
   )
 }
