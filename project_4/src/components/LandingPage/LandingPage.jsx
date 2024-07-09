abecd17dd8be90790ae3bb7fcea8f8034947564a
onslaught7 <krutibashmohapatra7@gmail.com>
2024-07-09 12:06:08 +0530

    added horizontal strikke using before and after pseudo elements

--- project_4/src/components/LandingPage/LandingPage.jsx
+++ project_4/src/components/LandingPage/LandingPage.jsx
@@ -6,26 +6,32 @@ import Button from '../Button/Button';
 
 const LandingPage = () => {
   return (
-    <div className={`${styles.ldnPage} container`}>
-      <div className={styles.greetings}>
-        <h1>YOUR INDIAN <GiLindenLeaf className={styles.leaf}/> CULINARY DREAM</h1>
-        <p>
-        Welcome to <img src="/images/logo_txt.png" alt="Restaurant Name"/> 
-        - a place which serves more than just a meal.
-        A feast that nourishes your hunger, heart, and soul. 
-        Experience culinary delights crafted with love, 
-        where every bite tells a story and every meal becomes a cherished memory. 
-        </p>
+    <div>
+      <div className={`${styles.ldnPage} container`}>
+        <div className={styles.greetings}>
+          <h1>YOUR INDIAN <GiLindenLeaf className={styles.leaf}/> CULINARY DREAM</h1>
+          <p>
+          Welcome to <img src="/images/logo_txt.png" alt="Restaurant Name"/> 
+          - a place which serves more than just a meal.
+          A feast that nourishes your hunger, heart, and soul. 
+          Experience culinary delights crafted with love, 
+          where every bite tells a story and every meal becomes a cherished memory. 
+          </p>
 
-        <Button 
-          buttonType="btn3"
-          icon={<LuMenuSquare />}
-          text={"ORDER NOW"}
-        />
+          <Button 
+            buttonType="btn3"
+            icon={<LuMenuSquare />}
+            text={"ORDER NOW"}
+          />
+        </div>
+
+        <div className={styles.greetImg}>
+          <img src="/images/food1.png"/>
+        </div>
       </div>
 
-      <div className={styles.greetImg}>
-        <img src="/images/food1.png"/>
+      <div className={styles.logo}>
+        <img src="/images/logo_txt.png"></img>
       </div>
     </div>
   )
