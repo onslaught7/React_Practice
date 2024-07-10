0e03fd688a56bbaf0168e11704088263698555d0
onslaught7 <krutibashmohapatra7@gmail.com>
2024-07-10 21:48:57 +0530

    styling the description page

--- project_4/src/components/Description/Description.jsx
+++ project_4/src/components/Description/Description.jsx
@@ -15,7 +15,15 @@ const Description = () => {
             
             <div className={styles.content}>
               <h1 className={styles.title}>ABOUT US</h1>
-              <p className={styles.description}></p>
+              <p className={styles.description}>
+              Welcome to our restaurant, where the essence of India comes alive in 
+              every bite. Here, we don't just serve food; we serve a tradition, a culture, 
+              and a story that spans generations. Our chefs craft each dish with authentic 
+              recipes and the freshest ingredients, ensuring every meal is a culinary masterpiece. 
+              We take pride in our rave reviews and loyal patrons who swear by our flavors. 
+              Come and taste for yourself why we're as exceptional as we claim to be. Your 
+              journey to India begins here.
+              </p>
               <Button
                 buttonType="btn3"
                 icon={<CgMoreVerticalR />}
