97ef6bf5a5de1303e072a170a2fc20e9c65153d4
onslaught7 <krutibashmohapatra7@gmail.com>
2024-07-10 10:14:28 +0530

    included more images

--- project_4/src/components/Description/Description.jsx
+++ project_4/src/components/Description/Description.jsx
@@ -7,8 +7,18 @@ const Description = () => {
     <div>
       <Navbar />
 
-      <div className={styles.descPage}>
-        
+      <div className={`${styles.descPage} container`}>
+        <div className={styles.descAboutSlide}>
+          <div>
+            <img />
+          </div>
+          <div></div>
+        </div>
+
+        <div className={styles.descPopularCardSlide}>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   )
