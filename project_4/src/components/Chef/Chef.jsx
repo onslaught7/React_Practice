082ccaab00e4a0dd5216b1f59beda6f68c92dba8
onslaught7 <krutibashmohapatra7@gmail.com>
2024-07-22 17:15:37 +0530

    chef page front end done

--- project_4/src/components/Chef/Chef.jsx
+++ project_4/src/components/Chef/Chef.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import styles from './Chef.module.css'
 
 const Chef = () => {
+  const chefs = ['chef1', 'chef2', 'chef3'];
+  const names = ['Chef Name One', 'Chef Name Two', 'Chef Name Three'];
+  const intro = [
+    'Head Chef', 
+    'Assistant Head Chef', 
+    'Assistant Head Chef'
+  ];
+
   return (
-    <div>Chefsfsdgdsgf</div>
+    <div className={`${styles.chef_container} container`}>
+      {
+        chefs.map((name, index) => 
+          <div className={styles.chef_profile}>
+              <img src={`/images/${name}.jpg`}/>
+              <div className={styles.chef_info}>
+                <h1>{names[index]}</h1>
+                <p>{intro[index]}</p>
+              </div>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
